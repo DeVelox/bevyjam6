091baeb88e4f86efae1dbbf20a3a444cf8d99d3c
DeVelox <DeVelox@users.noreply.github.com>
2025-06-08 17:17:13 +0200

    change game name

--- src/main.rs
+++ src/main.rs
@@ -38,7 +38,7 @@ impl Plugin for AppPlugin {
                 })
                 .set(WindowPlugin {
                     primary_window: Window {
-                        title: "Bevyjam6".to_string(),
+                        title: "Shift-O".to_string(),
                         fit_canvas_to_parent: true,
                         ..default()
                     }
